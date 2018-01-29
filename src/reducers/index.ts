abfb60763829490566aa98c7a7e6d5eadc42fdbc
Kia Farhang <kfarhang0@gmail.com>
2018-01-28 19:17:52 -0600

    Create basic reducer structure

---
+++ src/reducers/index.ts
@@ -0,0 +1,20 @@
+import * as actions from "../actions";
+import * as types from "../types";
+import { initialState } from "./initialState";
+
+const todos = (state: types.ITodo[] = [], action: actions.IAction): types.ITodo[] => {
+    switch (action.type) {
+        case "ADD_TODO":
+            const theAction = action as actions.IAddTodo;
+            return state.concat([{ text: theAction.text, completed: false }]);
+        default:
+            return state;
+    }
+};
+
+const todoApp = (state: types.IAppState = initialState, action: actions.IAction): types.IAppState => {
+    return {
+        filter: "SHOW_ALL",
+        todos: todos(state.todos, action),
+    };
+};
---
+++ src/reducers/initialState.ts
@@ -0,0 +1,6 @@
+import { IAppState } from "../types";
+
+export const initialState: IAppState = {
+    filter: "SHOW_ALL",
+    todos: [],
+};
